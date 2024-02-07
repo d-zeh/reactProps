ed8e74ff89ffc825945aeff012687ac8dbaa75a3
Daniel Zeh <dfzeh1@gmail.com>
2024-02-07 11:17:51 -0600

    commit

--- reactprops/src/index.js
+++ reactprops/src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Card() {
-  return <div>
-    <h1>My Contacts</h1>
+function Card(props) {
 
-    <h2>Beyonce</h2>
+  console.log(props)
+  return <div>
+
+    <h2>{props.name}</h2>
     <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      src={props.img}
       alt="avatar_img"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
+    <p>{props.tel}</p>
+    <p>{props.email}</p>
   </div>
 }
 
@@ -20,6 +21,9 @@ function Card() {
 ReactDOM.render(
   <div>
   <h1>My Contacts</h1>
+  <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="avatar_img"/>
+  <Card name="FF0026" img="https://assets.nierrein.guide/ui/costume/ch012012/ch012012_full.png" tel="flowers" email="kill the flowers" />
+  <input id="fname" placeholder="Enter you first name" value="Daniel Zeh"/>
 
 
   </div>,
